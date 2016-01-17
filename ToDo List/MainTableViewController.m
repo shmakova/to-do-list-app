ef132dcac46d734abc4c0c69e8362af82c9910da
shmakova <shmakova-nastya@yandex.ru>
2016-01-17 14:08:00 +0600

    Настройка удаления уведомлений

--- ToDo List/MainTableViewController.m
+++ ToDo List/MainTableViewController.m
@@ -77,25 +77,23 @@
     [self.navigationController pushViewController:detailView animated:YES];
 }
 
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+        UILocalNotification *notification = [self.arrayEvents objectAtIndex:indexPath.row];
+        [[UIApplication sharedApplication] cancelLocalNotification:notification];
+        [self.arrayEvents removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
 /*
 // Override to support rearranging the table view.
